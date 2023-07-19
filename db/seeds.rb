8497f618125a6b0127493f3b47718575077920e0
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-19 18:17:34 +0200

    Create the Greetings Controller
    rails db:create
    rails generate model Greeting message:string
    rails db:migrate
    rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello")
+Greeting.create(message: "Hi")
+Greeting.create(message: "Hey")
+Greeting.create(message: "Hola")
+Greeting.create(message: "Bonjour")
