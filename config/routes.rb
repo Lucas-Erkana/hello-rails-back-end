6ca1ca3887b4cc0ed7b4e33653a100d78dcc37ec
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-19 19:05:09 +0200

    Update routes
    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :greetings
-    end
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "api/v1/greetings#index"
 end
